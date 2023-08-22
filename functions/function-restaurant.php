5faeaf02fd72ecd4fbfaacea2e9e9a7dff116690
candagdelen0 <candagdelen0@gmail.com>
2023-08-22 16:44:49 +0300

    Güvenlik Fonksiyonu

--- functions/function-restaurant.php
+++ functions/function-restaurant.php
@@ -9,4 +9,11 @@
                 return $sonuc = $sor->get_result();
             endif;
         }
+
+        public function safety($data) {
+            $data = trim($data);
+            $data = stripcslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
     }
