ac8925460e468dee463443095982e897c69ffc42
candagdelen0 <candagdelen0@gmail.com>
2023-08-23 15:18:58 +0300

    Giriş Kontrol Fonksiyonu

--- functions/function-restaurant.php
+++ functions/function-restaurant.php
@@ -21,5 +21,21 @@
             echo '<div class="mt-3 alert alert-'.$tip .' ">'. $metin .' </div>';
             header('refresh:2, url='. $sayfa.' ');
         }
-
+        
+        public function giriskont($vt, $isim, $sifre, $tablo) {
+            $sifreli = md5(md5(sha1($sifre)));
+            $gelen = $this->genelsorgu($vt, "SELECT * FROM $tablo WHERE ad='$isim' AND sifre='$sifreli' ",1);
+            $gelenbilgi = $gelen->FETCH_ASSOC();
+            if ($gelen->num_rows == 0):
+                $this->uyari("danger","Bilgiler Hatalı","index.php");
+            else:
+                if ($tablo == "yonetim"):
+                    $_SESSION['Kullanici'] = $gelenbilgi["ad"];
+                    header("Location:yonetim.php");
+                else:
+                    $_SESSION['Kullanici'] = $gelenbilgi["ad"];
+                    header("Location:anasayfa.php");
+                endif;
+            endif;
+        }
     }
