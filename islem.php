59b0c3f90dbb05d558fdbea852123a9904c08148
candagdelen0 <candagdelen0@gmail.com>
2023-08-24 18:21:59 +0300

    İşlemler | sorgu fonksiyonu

--- islem.php
+++ islem.php
@@ -2,4 +2,17 @@
     require "functions/function-restaurant.php";
     include "partial/_header.php";
     @$masaid = $_GET["masaid"];
+
+
+    /* ***************************     FONKSİYONLAR     *********************** */
+    function benimsorgum($vt,$sorgu,$tercih) {
+        $b=$vt->prepare($sorgu);
+        $b->execute();
+        if ($tercih==1):
+            return $c=$b->get_result();  
+        endif;
+    }
+
+
+
 ?>
